556e3c7cd7d56f5c34747687886a09c06a91d1af
Linar <98088028+Nillud@users.noreply.github.com>
2025-09-08 17:36:11 +0500

    access and refresh tokens

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -4,15 +4,23 @@ import { UserModel } from 'src/user/user.model';
 import { ModelType } from '@typegoose/typegoose/lib/types';
 import { AuthDto } from './dto/auth.dto';
 import { hash, getSalt, compare, genSalt } from 'bcryptjs'
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
   constructor(
-    @InjectModel(UserModel) private readonly UserModel: ModelType<UserModel>
+    @InjectModel(UserModel) private readonly UserModel: ModelType<UserModel>,
+    private readonly jwtService: JwtService
   ) { }
 
   async login(dto: AuthDto) {
-    return this.validateUser(dto)
+    const user = await this.validateUser(dto)
+    const tokens = await this.issueTokenPair(String(user._id))
+
+    return {
+      user: this.returnUserFields(user),
+      ...tokens
+    }
   }
 
   async register(dto: AuthDto) {
@@ -24,7 +32,13 @@ export class AuthService {
       email: dto.email,
       password: await hash(dto.password, salt)
     })
-    return newUser.save()
+
+    const tokens = await this.issueTokenPair(String(newUser._id))
+
+    return {
+      user: this.returnUserFields(newUser),
+      ...tokens
+    }
   }
 
   async validateUser(dto: AuthDto): Promise<UserModel> {
@@ -35,5 +49,28 @@ export class AuthService {
     if (!isValidPassword) throw new UnauthorizedException('Invalid password')
 
     return user
+  }
+
+  async issueTokenPair(userId: string) {
+    const data = { _id: userId }
+
+    const refreshToken = await this.jwtService.signAsync(data, {
+      expiresIn: '15d'
+    })
+
+    const accessToken = await this.jwtService.signAsync(data, {
+      expiresIn: '1h'
+    })
+
+    return { refreshToken, accessToken }
+  }
+
+  returnUserFields(user: UserModel) {
+    return {
+      _id: user._id,
+      email: user.email,
+      isAdmin: user.isAdmin,
+      password: user.password
+    }
   }
 }
